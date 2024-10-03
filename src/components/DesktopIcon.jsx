405a173a7f4581163f9f955e277ce6ae5a147740
Haïtaro <oli.f.barbin@gmail.com>
2024-10-02 20:37:45 -0400

    Modification du DesktopIcon.jsx

--- src/components/DesktopIcon.jsx
+++ src/components/DesktopIcon.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import Draggable from 'react-draggable'; // Import de react-draggable
 import './DesktopIcon.css';
-import iconImage from '../assets/img/github.png'; // Assurez-vous que ce chemin est correct
+import iconImage from '../assets/img/github.png';  // Chemin vers l'icône
 
 const DesktopIcon = () => {
     return (
-        <div className="desktop-icon">
-            <img src={iconImage} alt="Github" className="icon-image" />
-            <div className="desktop-icon-label">Mon Github</div>
-        </div>
+        <Draggable>
+            <div className="desktop-icon">
+                <img src={iconImage} alt="Github" />
+                <div className="desktop-icon-label">Mon Github</div>
+            </div>
+        </Draggable>
     );
 };
 
